270638570191505e546e447d1e14fb8cec1c0ae0
Stephen Weiberg <sweiberg@ufl.edu>
2023-09-30 20:34:42 -0400

    Initial commit

---
+++ src/main/java/plc/project/Lexer.java
@@ -0,0 +1,210 @@
+package plc.project;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * The lexer works through three main functions:
+ *
+ *  - {@link #lex()}, which repeatedly calls lexToken() and skips whitespace
+ *  - {@link #lexToken()}, which lexes the next token
+ *  - {@link CharStream}, which manages the state of the lexer and literals
+ *
+ * If the lexer fails to parse something (such as an unterminated string) you
+ * should throw a {@link ParseException} with an index at the character which is
+ * invalid or missing.
+ *
+ * The {@link #peek(String...)} and {@link #match(String...)} functions are
+ * helpers you need to use, they will make the implementation a lot easier.
+ */
+public final class Lexer {
+
+    private final CharStream chars;
+    private final List<Token> tokens = new ArrayList<>();
+
+    public Lexer(String input) {
+        chars = new CharStream(input);
+    }
+
+    /**
+     * Repeatedly lexes the input using {@link #lexToken()}, also skipping over
+     * whitespace where appropriate.
+     */
+    public List<Token> lex() {
+        while (chars.has(0)) {
+            if (match("[\b\r\n\t\\s]")) {
+                chars.skip();
+            }
+            else {
+                tokens.add(lexToken());
+            }
+        }
+
+        return tokens;
+    }
+
+    /**
+     * This method determines the type of the next token, delegating to the
+     * appropriate lex method. As such, it is best for this method to not change
+     * the state of the char stream (thus, use peek not match).
+     *
+     * The next character should start a valid token since whitespace is handled
+     * by {@link #lex()}
+     */
+    public Token lexToken() {
+        if (match("[A-Za-z_]")) return lexIdentifier();
+        else if (peek("\\d")) return lexNumber();
+        else if (peek("\\+", "\\d") || peek("\\-", "\\d")) return lexNumber();
+        else if (peek("'")) return lexCharacter();
+        else if (match("\"")) return lexString();
+        else return lexOperator();
+    }
+
+    public Token lexIdentifier() {
+        while (peek("[A-Za-z0-9_-]")) {
+            chars.advance();
+        }
+
+        return chars.emit(Token.Type.IDENTIFIER);
+    }
+
+    public Token lexNumber() {
+        boolean decimal = false;
+
+        if (peek("[+-]")) chars.advance();
+
+        while (peek("[0-9.]")) {
+            if (peek("[.]")) {
+                if (!decimal && chars.length > 1) decimal = true;
+                else throw new ParseException("Error: Trailing Or Multiple Decimal", chars.index);
+            }
+
+            chars.advance();
+        }
+
+        if (decimal) return chars.emit(Token.Type.DECIMAL);
+
+        return chars.emit(Token.Type.INTEGER);
+    }
+
+    public Token lexCharacter() {
+        if (match( "'", ".", "'")) {
+            return chars.emit(Token.Type.CHARACTER);
+        }
+        else if (match( "'", "\\\\", "[brnt'\"\\\\]", "'")) {
+            return chars.emit(Token.Type.CHARACTER);
+        }
+        else {
+            throw new ParseException("Error: Exceeds Character Limit", chars.index);
+        }
+    }
+
+    public Token lexString() {
+        while (!peek("\"") && chars.has(0)) {
+            if (match("\\\\") && !match("[bnrt'\"\\\\]")) {
+                throw new ParseException("Error: Invalid Escape", chars.index);
+            }
+            else {
+                chars.advance();
+            }
+        }
+
+        if (!chars.has(0)) {
+            throw new ParseException("Error: Unterminated String", chars.index);
+        }
+
+        chars.advance();
+
+        return chars.emit(Token.Type.STRING);
+    }
+
+    public void lexEscape() {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    public Token lexOperator() {
+        if (match("[!=<>]", "=")) {
+            return chars.emit(Token.Type.OPERATOR);
+        }
+
+        chars.advance();
+
+        return chars.emit(Token.Type.OPERATOR);
+    }
+
+    /**
+     * Returns true if the next sequence of characters match the given patterns,
+     * which should be a regex. For example, {@code peek("a", "b", "c")} would
+     * return true if the next characters are {@code 'a', 'b', 'c'}.
+     */
+    public boolean peek(String... patterns) {
+        for (int i = 0; i < patterns.length; i++) {
+            if (!chars.has(i) || !String.valueOf(chars.get(i)).matches(patterns[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Returns true in the same way as {@link #peek(String...)}, but also
+     * advances the character stream past all matched characters if peek returns
+     * true. Hint - it's easiest to have this method simply call peek.
+     */
+    public boolean match(String... patterns) {
+        boolean peek = peek(patterns);
+
+        if (peek) {
+            for (int i = 0; i < patterns.length; i++) {
+                chars.advance();
+            }
+        }
+
+        return peek;
+    }
+
+    /**
+     * A helper class maintaining the input string, current index of the char
+     * stream, and the current length of the token being matched.
+     *
+     * You should rely on peek/match for state management in nearly all cases.
+     * The only field you need to access is {@link #index} for any {@link
+     * ParseException} which is thrown.
+     */
+    public static final class CharStream {
+
+        private final String input;
+        private int index = 0;
+        private int length = 0;
+
+        public CharStream(String input) {
+            this.input = input;
+        }
+
+        public boolean has(int offset) {
+            return index + offset < input.length();
+        }
+
+        public char get(int offset) {
+            return input.charAt(index + offset);
+        }
+
+        public void advance() {
+            index++;
+            length++;
+        }
+
+        public void skip() {
+            length = 0;
+        }
+
+        public Token emit(Token.Type type) {
+            int start = index - length;
+            skip();
+            return new Token(type, input.substring(start, index), start);
+        }
+
+    }
+
+}
